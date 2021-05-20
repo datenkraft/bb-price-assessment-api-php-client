e40ee7bac9b5788b9ff8afc2462eb5667d3dfa39
Raphael Pansi <raphael.pansi@datenkraft.com>
2021-05-20 16:16:21 +0200

    BB-155 Setup bb-price-assessment-api-php-client repository (README, LICENSE, etc.)

---
+++ jane-openapi-configuration.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'openapi-file' => __DIR__ . '/openapi.json',
+    'namespace' => 'Datenkraft\Backbone\Client\PriceAssessmentApi\Generated',
+    'directory' => __DIR__ . '/src/Generated',
+    'throw-unexpected-status-code' => true,
+];
