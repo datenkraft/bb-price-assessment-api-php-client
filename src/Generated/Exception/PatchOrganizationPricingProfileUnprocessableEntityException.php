3ab6362a2806fb9f47a3b68e1a4ce97dcdc8b5da
CircleCI <office@datenkraft.com>
2023-03-23 11:37:59 +0000

    Autogenerated client update [publish]

---
+++ src/Generated/Exception/PatchOrganizationPricingProfileUnprocessableEntityException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Datenkraft\Backbone\Client\PriceAssessmentApi\Generated\Exception;
+
+class PatchOrganizationPricingProfileUnprocessableEntityException extends UnprocessableEntityException
+{
+    /**
+     * @var \Datenkraft\Backbone\Client\PriceAssessmentApi\Generated\Model\ErrorResponse
+     */
+    private $errorResponse;
+    public function __construct(\Datenkraft\Backbone\Client\PriceAssessmentApi\Generated\Model\ErrorResponse $errorResponse)
+    {
+        parent::__construct('Unprocessable Entity');
+        $this->errorResponse = $errorResponse;
+    }
+    public function getErrorResponse() : \Datenkraft\Backbone\Client\PriceAssessmentApi\Generated\Model\ErrorResponse
+    {
+        return $this->errorResponse;
+    }
+}
