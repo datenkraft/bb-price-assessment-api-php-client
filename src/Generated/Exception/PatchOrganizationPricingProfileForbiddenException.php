3d9a7a10359d3b49e7b2d7ed8e0ed6f0f4a20211
CircleCI <office@datenkraft.com>
2023-03-31 11:28:48 +0000

    Autogenerated client update [publish]

---
+++ src/Generated/Exception/PatchOrganizationPricingProfileForbiddenException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Datenkraft\Backbone\Client\PriceAssessmentApi\Generated\Exception;
+
+class PatchOrganizationPricingProfileForbiddenException extends ForbiddenException
+{
+    /**
+     * @var \Datenkraft\Backbone\Client\PriceAssessmentApi\Generated\Model\ErrorResponse
+     */
+    private $errorResponse;
+    public function __construct(\Datenkraft\Backbone\Client\PriceAssessmentApi\Generated\Model\ErrorResponse $errorResponse)
+    {
+        parent::__construct('Forbidden');
+        $this->errorResponse = $errorResponse;
+    }
+    public function getErrorResponse() : \Datenkraft\Backbone\Client\PriceAssessmentApi\Generated\Model\ErrorResponse
+    {
+        return $this->errorResponse;
+    }
+}
